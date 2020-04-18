8b9d911b4efba5f4d18318c3c226c6f9eecde09f
cleiton016 <cleitonluiz2014@gmail.com>
2020-04-18 01:46:18 -0300

    Formulario Bootstrap com verificação

--- CadastroComValidacao/src/app/app.component.ts
+++ CadastroComValidacao/src/app/app.component.ts
@@ -8,14 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit{
   title = 'CadastroComValidacao';
   nome = null;  data = null;  sexo = null;  cpf = null;  email = null;  telefone = null;
-  endereco = null;  cep = null; cidade = null;  documentos = null;
+  endereco = null;  cep = null; cidade = null;  documentos = null; ufSelect = null; turmasSelect = null;
+//-------------------------------------------------------------------------------------\\
   turmas = ['AP1', 'AP2', 'LPWEB', 'LPC'];
-  //uf = ['AC','AL','AP','AM','BA','DF','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE'
-  //];
-  uf = null;
+  uf: any;
+  listaDeCadastrados = [];
   constructor(private ufs: UfService) {}
-  ngOnInit(): void {
-    this.ufs.allUf().subscribe((dados: any) => this.uf = dados);
+  ngOnInit(): void {this.ufs.allUf().subscribe((dados: any) => this.uf = dados);}
+  cadastrar() {
+    this.listaDeCadastrados.push({nome: this.nome, dataNascimento: this.data, sexo: this.sexo,
+                                  cpf: this.cpf, email: this.email, telefone: this.telefone,
+                                endereco: this.endereco, cep: this.cep, cidade: this.cidade, documentos: this.documentos,
+                              uf: this.ufSelect, turmas: this.turmasSelect})
+    this.limpaForm();
+    console.log(this.listaDeCadastrados);
   }
-
+  limpaForm() {
+    this.nome = null;  this.data = null;  this.sexo = null;  this.cpf = null;  this.email = null;  this.telefone = null;
+    this.endereco = null;  this.cep = null; this.cidade = null;  this.documentos = null; this.ufSelect = null;
+  }
 }
