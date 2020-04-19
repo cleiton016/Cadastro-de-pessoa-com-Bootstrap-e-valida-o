4f4d126ff439bdf7bec280834fde05cf63e94c50
cleiton016 <cleitonluiz2014@gmail.com>
2020-04-18 23:38:59 -0300

    Exibição de Alunos

--- CadastroComValidacao/src/app/app.component.ts
+++ CadastroComValidacao/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { UfService } from './uf.service';
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+import { getLocaleDateFormat } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,19 +14,34 @@ export class AppComponent implements OnInit{
 //-------------------------------------------------------------------------------------\\
   turmas = ['AP1', 'AP2', 'LPWEB', 'LPC'];
   uf: any;
-  listaDeCadastrados = [];
+  listaDeCadastrados = [{nome: "Cleiton Luiz de Paula Barbosa",
+  dataNascimento: '2000-02-20',
+  sexo: "Masculino",
+  cpf: "050.907.221-61",
+  email: "cleitonluiz2014@gmail.com",
+  telefone: "63992198455",
+  endereco: "Quadra ARSE 82 Alameda 20         ",
+  cep: "77023100",
+  cidade: "Palmas",
+  documentos: true,
+  uf: "TO",
+  turmas:["LPWEB", "LPC"]}
+  ];
   constructor(private ufs: UfService) {}
-  ngOnInit(): void {this.ufs.allUf().subscribe((dados: any) => this.uf = dados);}
-  cadastrar() {
+  ngOnInit(): void {this.ufs.allUf().subscribe((dados: any) => this.uf = dados); }
+  cadastrar(form) {
     this.listaDeCadastrados.push({nome: this.nome, dataNascimento: this.data, sexo: this.sexo,
                                   cpf: this.cpf, email: this.email, telefone: this.telefone,
-                                endereco: this.endereco, cep: this.cep, cidade: this.cidade, documentos: this.documentos,
-                              uf: this.ufSelect, turmas: this.turmasSelect})
-    this.limpaForm();
-    console.log(this.listaDeCadastrados);
+                                  endereco: this.endereco, cep: this.cep, cidade: this.cidade, documentos: this.documentos,
+                                  uf: this.ufSelect, turmas: this.turmasSelect});
+    form.reset();
   }
-  limpaForm() {
-    this.nome = null;  this.data = null;  this.sexo = null;  this.cpf = null;  this.email = null;  this.telefone = null;
-    this.endereco = null;  this.cep = null; this.cidade = null;  this.documentos = null; this.ufSelect = null;
+  limpaForm(form) {
+    form.reset();
+  }
+  getIdade(data: any){
+    const now = new Date;
+    const ano = new Date(data);
+    return now.getFullYear() - ano.getFullYear();
   }
 }
